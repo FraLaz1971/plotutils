52b663578ad479c9328721a107136fb68b338b24
Francesco Lazzarotto <fralaz1971@gmail.com>
2020-07-19 17:54:06 +0200

    updated png support cpp -D switch

--- local-cross-configure-win.sh
+++ local-cross-configure-win.sh
@@ -1,7 +1,7 @@
 CC="i686-w64-mingw32-gcc"
 CXX="i686-w64-mingw32-g++"
 export CFLAGS="-DDEBUG=1 -DPNG_PTR=1 -DINCLUDE_PNG_SUPPORT=1 -DHAVE_LIBPNG=1  -I/mingw32/include -Iinclude -I/mingw32/include -DMINGW=1"
-export CPPFLAGS="-DDEBUG=1 -DPNG_PTR=1 -DINCLUDE_PNG_SUPPORT  -DHAVE_LIBPNG=1  -I/mingw32/include -I/mingw32/include/ -Iinclude -DMINGW=1 -DGNUC=1"
+export CPPFLAGS="-DDEBUG=1 -DPNG_PTR=1 -DINCLUDE_PNG_SUPPORT=1  -DHAVE_LIBPNG=1  -I/mingw32/include -I/mingw32/include/ -Iinclude -DMINGW=1 -DGNUC=1"
 export CXXFLAGS="-DDEBUG=1 -DPNG_PTR=1 -DINCLUDE_PNG_SUPPORT=1 -DHAVE_LIBPNG=1  -I/mingw32/include -I/mingw32/include/libpng16 -Iinclude -DMINGW=1"
 export LDFLAGS="-L/mingw32/lib -L/mingw32/bin"
 export LIBS=""
