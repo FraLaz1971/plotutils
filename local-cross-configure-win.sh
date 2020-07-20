4c6c36354050450bbe4873ef3d5d16d32c2e6156
Francesco Lazzarotto <fralaz1971@gmail.com>
2020-07-20 22:29:45 +0200

    forced configure.ac to accept libpng

--- local-cross-configure-win.sh
+++ local-cross-configure-win.sh
@@ -1,15 +1,14 @@
-export CC=""; export CXX=""; export CPPFLAGS=""; 
-export CFLAGS=""; export CXXFLAGS=""; 
-export CC="i686-w64-mingw32-gcc"
-export CXX="i686-w64-mingw32-g++"
-export CPPFLAGS="-Iinclude -I/mingw32/include -I. -I.."
 ./configure \
---build=x86_64-unknown-linux-gnu \
 --host=i686-w64-mingw32 \
 --target=i686-w64-mingw32 \
---with-gnu-ld \
---enable-libplotter \
---enable-libxmi \
+--prefix=/mingw32 \
 --with-libpng \
 --without-x \
---prefix=/mingw32 \
+--enable-libplotter \
+--enable-libxmi \
+CC="i686-w64-mingw32-gcc" \
+CXX="i686-w64-mingw32-g++" \
+CPPFLAGS="-DDEBUG=1 -DPNG_PTR=1 -DINCLUDE_PNG_SUPPORT  -DHAVE_LIBPNG=1  -I/mingw32/include -I/mingw32/include/ -Iinclude -DMINGW=1 -DGNUC=1 " \
+CFLAGS="-DDEBUG=1 -DPNG_PTR=1 -DINCLUDE_PNG_SUPPORT=1 -DHAVE_LIBPNG=1  -I/mingw32/include -Iinclude -I/mingw32/include -DMINGW=1" \
+CXXFLAGS="-DDEBUG=1 -DPNG_PTR=1 -DINCLUDE_PNG_SUPPORT=1 -DHAVE_LIBPNG=1  -I/mingw32/include -I/mingw32/include/libpng16 -Iinclude -DMINGW=1 " \
+LDFLAGS="-L/mingw32/lib/libpng16"
