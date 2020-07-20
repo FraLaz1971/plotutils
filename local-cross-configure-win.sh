1745c729586da69fbf8e78f47d1456e08a2d2e05
Francesco Lazzarotto <fralaz1971@gmail.com>
2020-07-20 18:51:02 +0200

    added config.status

--- local-cross-configure-win.sh
+++ local-cross-configure-win.sh
@@ -1,15 +1,15 @@
-export CC=""; export CXX=""; export CPPFLAGS=""; export LDFLAGS=""
-export CFLAGS=""; export CXXFLAGS=""; export AM_LDFLAGS=""
+export CC=""; export CXX=""; export CPPFLAGS=""; 
+export CFLAGS=""; export CXXFLAGS=""; 
 export CC="i686-w64-mingw32-gcc"
 export CXX="i686-w64-mingw32-g++"
-export CPPFLAGS="-Iinclude -I/mingw32/include"
-export AM_LDFLAGS="-L/mingw32/bin -L/mingw32/lib"
+export CPPFLAGS="-Iinclude -I/mingw32/include -I. -I.."
 ./configure \
---build=x86_64-ubuntu-linux-gnu \
+--build=x86_64-unknown-linux-gnu \
 --host=i686-w64-mingw32 \
+--target=i686-w64-mingw32 \
+--with-gnu-ld \
 --enable-libplotter \
 --enable-libxmi \
 --with-libpng \
 --without-x \
 --prefix=/mingw32 \
---with-sysroot=/mingw32
